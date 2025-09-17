c0df377a343999c84039c6352c65f183a162e72b
Lee, Yurim <yurime0507@gmail.com>
2025-09-17 15:39:32 +0900

    six

--- src/d202417011/Main.java
+++ src/d202417011/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		System.out.println("이유림");
-		System.out.println("안녕하세요");
-		Hello.run();
+		System.out.println("안녕하세요");		
+		System.out.println("hello world");
 	}
 }
